f3abc774c66a5e2e46615853e9582c95b745cb67
Evan Cooper <esccoding@gmail.com>
2025-09-10 15:06:34 -0400

    added exception handling to `DirectoryHandler.get_subdirectories()`, and renamed `add_subdirectory_to_path()` to `add_to_path()`

--- src/core/DirectoryHandler.py
+++ src/core/DirectoryHandler.py
@@ -1,10 +1,20 @@
 from pathlib import Path
 
 from common.Singleton import Singleton
+from utils.Logger import Logger
 
 class DirectoryHandler(metaclass=Singleton):
     def get_subdirectories(directory: Path) -> list[str]:
-        return [folder.name for folder in Path(directory).iterdir() if folder.is_dir()]
+        try:
+            __target_subdirectories = [folder.name for folder in Path(directory).iterdir() if folder.is_dir()]
+            return __target_subdirectories
+        
+        except FileNotFoundError:
+            Logger.log_message("error", f"Unable to collect subdirectories from '{directory}' -- path does not exist")
+            raise FileNotFoundError
+        
+        except Exception as e:
+            Logger.log_message("critical", f"Unexpected exception encountered in DirectoryHandler.get_subdirectories(): {e}")
     
-    def add_subdirectory_to_path(directory: Path, subdirectory: str) -> Path:
+    def add_to_path(directory: Path, subdirectory: str) -> Path:
         return Path(str(directory) + f"\\{subdirectory}")
